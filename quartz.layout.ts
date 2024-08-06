2329377bbc51d2d5be1ccbf449dc4b1c68854275
Rouhollah Ahmadi <R.Ahmadi89@gmail.com>
2024-08-06 15:39:22 +0330

    Quartz sync: Aug 6, 2024, 3:39 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,6 +17,7 @@ export const sharedPageComponents: SharedLayout = {
       category: 'Announcements',
       // from data-category-id
       categoryId: 'DIC_kwDOMfa5tM4Chbra',
+	  data-lang="fa",
     }
   }),
 ],
