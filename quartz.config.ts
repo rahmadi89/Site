22ed3c178a7b226fff810476d699f8995e706322
rahmadi89 <r.ahmadi89@gmail.com>
2025-04-27 13:24:52 +0330

    Quartz sync: Apr 27, 2025, 1:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "وبسایت شخصی روح اله احمدی",
+    pageTitle: "وبسایت شخصی روحاله احمدی",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
